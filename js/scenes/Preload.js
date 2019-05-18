a9e6a836720bcd9184eb435f2af3a83ffe75a87e
Golen <golen87@hotmail.com>
2019-05-18 06:59:09 +0200

    Convert tutorial into class

--- js/scenes/Preload.js
+++ js/scenes/Preload.js
@@ -15,6 +15,12 @@ class Preload extends Phaser.Scene {
 		);
 
 		this.load.audio('pop', [ 'assets/audio/pop.ogg', 'assets/audio/pop.mp3' ] );
+
+		this.load.image('sky', 'assets/images/sky.png');
+		this.load.image('ground', 'assets/images/platform.png');
+		this.load.image('star', 'assets/images/star.png');
+		this.load.image('bomb', 'assets/images/bomb.png');
+		this.load.spritesheet('dude', 'assets/images/dude.png', { frameWidth: 32, frameHeight: 48 });
 	}
 
 	create() {
